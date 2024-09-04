46600324497f8cc9bb9aab643357eabec13ecf34
OnlyF0uR <29165327+OnlyF0uR@users.noreply.github.com>
2024-09-04 20:33:05 +0200

    Initial code commit

---
+++ Eidolon/Eidolon.cpp
@@ -0,0 +1,37 @@
+﻿// CSImageOptimizer.cpp : Defines the entry point for the application.
+// 
+
+#include "Eidolon.h"
+
+static void compress(const cv::Mat& input, const std::string& outputFilePath) {
+    std::vector<int> compression_params;
+
+    compression_params.push_back(cv::IMWRITE_WEBP_QUALITY);
+    compression_params.push_back(80);
+
+    if (!cv::imwrite(outputFilePath, input, compression_params)) {
+        std::cerr << "Error: Could not write the image to " << outputFilePath << std::endl;
+    }
+    else {
+        std::cout << "Image successfully converted to " << outputFilePath << std::endl;
+    }
+}
+
+int main() {
+    std::string inputFilePath = "image.png";
+    std::string outputFilePath = "image.webp";
+
+    // Load the image from the file
+    cv::Mat image = cv::imread(inputFilePath, cv::IMREAD_COLOR);
+
+    // Check if the image was loaded successfully
+    if (image.empty()) {
+        std::cerr << "Error: Could not open or find the image " << inputFilePath << std::endl;
+        return -1;
+    }
+
+    // Compress and save the image
+    compress(image, outputFilePath);
+
+    return 0;
+}
---
+++ Eidolon/Eidolon.h
@@ -0,0 +1,7 @@
+﻿// Eidolon.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+#include <opencv2/opencv.hpp>
